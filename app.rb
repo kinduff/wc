25b88aa9e518fe9b477b27654aa836ee7c77267f
kinduff <abarcadabra@gmail.com>
2014-04-09 00:23:10 -0500

    Modula vista process

--- app.rb
+++ app.rb
@@ -5,6 +5,20 @@ end
 
 post '/process' do
   imagen = params[:imagen][0]
+  process_image imagen
+end
+
+post '/download' do
+  file = params[:imagen]
+  send_file file,
+    :filename => 'descarga.png',
+    :disposition => 'attachment',
+    :stream => false,
+    :type => 'image/png',
+    :status => 200
+end
+
+def process_image imagen
   formats = ['.png', '.jpg', '.jpeg']
   if formats.include? File.extname(imagen[:filename]).downcase
     original = 'tmp/'+imagen[:filename]
@@ -18,14 +32,4 @@ post '/process' do
   else
     status 400
   end
-end
-
-post '/download' do
-  file = params[:imagen]
-  send_file file,
-    :filename => 'descarga.png',
-    :disposition => 'attachment',
-    :stream => false,
-    :type => 'image/png',
-    :status => 200
 end
